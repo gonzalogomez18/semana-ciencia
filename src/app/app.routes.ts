9fc488d9e5d692993ef9d6b0fea93b607169aea2
Gonzalo G.S <ggomezs16@educarex.es>
2025-11-05 09:33:05 +0100

    funcionamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Juego } from './juego/juego';
+import { Funcionamiento } from './pages/funcionamiento/funcionamiento';
 
 export const routes: Routes = [
-    {path: 'juego', component: Juego}
+    {path: 'juego', component: Juego},
+    {path: '', component: Funcionamiento}
 ];
